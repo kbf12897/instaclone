e1a65cee98043f1d4da62d99c6e0bf162502b735
Karl Felter <kbf12897@gmail.com>
2022-03-15 14:04:17 -0400

    more seeder

--- app/seeds/users.py
+++ app/seeds/users.py
@@ -4,11 +4,11 @@ from app.models import db, User
 # Adds a demo user, you can add other users here if you want
 def seed_users():
     demo = User(
-        username='Demo', email='demo@aa.io', password='password')
+        username='Demo', email='demo@aa.io', profile_img='https://media-cldnry.s-nbcnews.com/image/upload/t_nbcnews-fp-1200-630,f_auto,q_auto:best/MSNBC/Components/Photo/_new/12025-ChuckPhoto-hmed-0950a.jpg' ,password='password')
     marnie = User(
-        username='marnie', email='marnie@aa.io', password='password')
+        username='marnie', email='marnie@aa.io', profile_img='https://cdn.myanimelist.net/images/characters/7/282923.jpg', password='password')
     bobbie = User(
-        username='bobbie', email='bobbie@aa.io', password='password')
+        username='bobbie', email='bobbie@aa.io', profile_img='https://food.fnr.sndimg.com/content/dam/images/food/editorial/talent/bobby-flay/FN-TalentAvatar-Bobby-Flay-colorblock.jpg.rend.hgtvcom.616.616.suffix/1531174427795.jpeg' ,password='password')
 
     db.session.add(demo)
     db.session.add(marnie)
