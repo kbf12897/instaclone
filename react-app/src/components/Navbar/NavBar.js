3ae9aa89c056009264ca3149253ab652a7daedcc
Karl Felter <kbf12897@gmail.com>
2022-03-16 17:03:57 -0400

    profile button

--- react-app/src/components/Navbar/NavBar.js
+++ react-app/src/components/Navbar/NavBar.js
@@ -1,28 +1,28 @@
 import React from 'react';
 import { useSelector } from 'react-redux';
 import { NavLink } from 'react-router-dom';
-import LogoutButton from '../auth/LogoutButton';
+import ProfileButton from './ProfileButton';
 import PostFormModal from '../NewPosts/PostsFormModal';
 import './NavBar.css';
 
 const NavBar = () => {
   const sessionUser = useSelector((state) => state.session.user);
 
-  let sessionLinks;
-  if (sessionUser) {
-    sessionLinks = <LogoutButton />;
-  } else {
-    sessionLinks = (
-      <div className='login-signup-links'>
-        <NavLink to='/login' exact={true} activeClassName='active'>
-            Login
-        </NavLink>
-        <NavLink to='/sign-up' exact={true} activeClassName='active'>
-            Sign Up
-        </NavLink>
-      </div>
-    )
-  }
+  // let sessionLinks;
+  // if (sessionUser) {
+  //   sessionLinks = <LogoutButton />;
+  // } else {
+  //   sessionLinks = (
+  //     <div className='login-signup-links'>
+  //       <NavLink to='/login' exact={true} activeClassName='active'>
+  //           Login
+  //       </NavLink>
+  //       <NavLink to='/sign-up' exact={true} activeClassName='active'>
+  //           Sign Up
+  //       </NavLink>
+  //     </div>
+  //   )
+  // }
 
   return (
     <nav className='nav-container'>
@@ -33,9 +33,16 @@ const NavBar = () => {
           </NavLink>
         </li>
         <div className='navbar-right-container'>
-          <PostFormModal />
+          <div className='home-button-right'>
+            <NavLink to='/' exact={true} activeClassName='active'>
+              <svg aria-label="Home" class="_8-yf5 " color="#262626" fill="#262626" height="24" role="img" viewBox="0 0 24 24" width="24"><path d="M22 23h-6.001a1 1 0 01-1-1v-5.455a2.997 2.997 0 10-5.993 0V22a1 1 0 01-1 1H2a1 1 0 01-1-1V11.543a1.002 1.002 0 01.31-.724l10-9.543a1.001 1.001 0 011.38 0l10 9.543a1.002 1.002 0 01.31.724V22a1 1 0 01-1 1z"></path></svg>
+            </NavLink>
+          </div>
+          <div className='new-post-button'>
+            <PostFormModal />
+          </div>
           <li className='session-links'>
-            {sessionLinks}
+            <ProfileButton />
           </li>
           </div>
       </ul>
