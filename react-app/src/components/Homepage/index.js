bae74a5559b6612ba7f64769ed5e5a1ebdf483c3
Karl Felter <kbf12897@gmail.com>
2022-03-18 12:11:46 -0400

    caption edit fix

--- react-app/src/components/Homepage/index.js
+++ react-app/src/components/Homepage/index.js
@@ -51,7 +51,7 @@ const Homepage = () => {
                                     {!showEdit && <div className='post-caption'>{post?.caption}</div>}
                                     {(showEdit && post?.id === postId) && <EditCaptionForm post={post} setShowEdit={setShowEdit}/>}
                                 </div>
-                                {sessionUser.id === Number(post?.id) && <div className='caption-three-dots' onClick={() => setEditPost(post?.id, !showEdit)}><svg aria-label="More options" className="_8-yf5 " color="#262626" fill="#262626" height="24" role="img" viewBox="0 0 24 24" width="24"><circle cx="12" cy="12" r="1.5"></circle><circle cx="6" cy="12" r="1.5"></circle><circle cx="18" cy="12" r="1.5"></circle></svg></div>}
+                                {sessionUser?.id === Number(post?.user_id) && <div className='caption-three-dots' onClick={() => setEditPost(post?.id, !showEdit)}><svg aria-label="More options" className="_8-yf5 " color="#262626" fill="#262626" height="24" role="img" viewBox="0 0 24 24" width="24"><circle cx="12" cy="12" r="1.5"></circle><circle cx="6" cy="12" r="1.5"></circle><circle cx="18" cy="12" r="1.5"></circle></svg></div>}
                             </div>
                             <Comments post={post}/>
                             <CommentForm post={post} />
