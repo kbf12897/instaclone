f54cbe50d54d409b7d4c21fc327a72ee09e79943
Karl Felter <kbf12897@gmail.com>
2022-03-23 19:40:48 -0400

    starting s3

--- app/forms/comment_form.py
+++ app/forms/comment_form.py
@@ -1,11 +1,16 @@
 from flask_wtf import FlaskForm
 from sqlalchemy import Integer
 from wtforms import StringField, IntegerField, SubmitField
-from wtforms.validators import DataRequired
+from wtforms.validators import DataRequired, ValidationError
+
+def comment_body_length(form, field):
+    comment_body = field.data
+    if len(comment_body) > 255:
+        raise ValidationError('Comment must be 255 characters or less.')
 
 class CommentForm(FlaskForm):
     user_id = IntegerField('user_id')
     post_id = IntegerField('post_id')
-    comment_body = StringField('comment_body', validators=[DataRequired()])
+    comment_body = StringField('comment_body', validators=[DataRequired(), comment_body_length])
 
     submit = SubmitField('submit')
