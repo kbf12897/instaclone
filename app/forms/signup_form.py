fd5033b4b8abae8513752ae5583e568082846020
Karl Felter <kbf12897@gmail.com>
2022-03-23 17:22:59 -0400

    valid email

--- app/forms/signup_form.py
+++ app/forms/signup_form.py
@@ -3,7 +3,7 @@ from flask_wtf import FlaskForm
 from wtforms import StringField, PasswordField
 from wtforms.validators import DataRequired, ValidationError
 from app.models import User
-
+import re
 
 def user_exists(form, field):
     # Checking if user exists
@@ -20,9 +20,16 @@ def username_exists(form, field):
     if user:
         raise ValidationError('Username is already in use.')
 
+def valid_email(form, field):
+    regex = re.compile('^\w+@[a-zA-Z_]+?\.[a-zA-Z]{2,3}$')
+    email = field.data
+    if not regex.match(email):
+        raise ValidationError('Please provide a valid email')
+
+
 
 class SignUpForm(FlaskForm):
     username = StringField(
         'username', validators=[DataRequired(), username_exists])
-    email = StringField('email', validators=[DataRequired(), user_exists])
+    email = StringField('email', validators=[DataRequired(), user_exists, valid_email])
     password = PasswordField('password', validators=[DataRequired()])
