2d056c06ee7dd1d1245c67900e24892ee325749d
Karl Felter <kbf12897@gmail.com>
2022-03-24 12:43:51 -0400

    seeds

--- app/seeds/comments.py
+++ app/seeds/comments.py
@@ -3,18 +3,28 @@ from app.models import db, Comment
 def seed_comments():
     post_1_comment_1 = Comment(
         user_id=2,
-        post_id=1,
+        post_id=4,
+        comment_body="He's so cute!"
+    )
+    post_1_comment_2 = Comment(
+        user_id=4,
+        post_id=4,
+        comment_body="I want one!"
+    )
+    post_1_comment_3 = Comment(
+        user_id=3,
+        post_id=4,
+        comment_body="Australian shephards are my favorite"
+    )
+    post_1_comment_4 = Comment(
+        user_id=6,
+        post_id=4,
         comment_body="I've got to visit one day!"
     )
-    post_2_comment_1 = Comment(
-        user_id=1,
-        post_id=2,
-        comment_body="Keep up the good work!"
-    )
-    post_3_comment_1 = Comment(
-        user_id=2,
-        post_id=3,
-        comment_body="He's so cute!"
+    post_1_comment_5 = Comment(
+        user_id=7,
+        post_id=4,
+        comment_body="I've got to visit one day!"
     )
 
 
